19061ed82765acf6fb7f875963b5d18ef81629b1
judithher <Judith.Hershko@student.uantwerpen.be>
2023-02-18 20:39:18 +0100

    start

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ export PYTHONPATH=$PYTHONPATH:src
 if [[ ! -f "antlr-4.8-complete.jar" ]]; then
   wget https://www.antlr.org/download/antlr-4.8-complete.jar
 fi;
-
+#generate visitor and listener file
 antlr4 -Dlanguage=Python3 Expr.g4 -visitor;
+#execute main
 python3 main.py Expr.g4
