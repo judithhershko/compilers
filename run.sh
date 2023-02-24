5e87d9dd2e3926bfa5ce38eec89bd7ea414dd66c
judithher <Judith.Hershko@student.uantwerpen.be>
2023-02-24 14:37:35 +0100

    start

--- run.sh
+++ run.sh
@@ -3,12 +3,9 @@
 # start folder=compilers
 export PYTHONPATH=$PYTHONPATH:src
 
-# Downloads the jar if it does not exists
-if [[ ! -f "antlr-4.8-complete.jar" ]]; then
-  wget https://www.antlr.org/download/antlr-4.8-complete.jar
-fi;
+
 #generate visitor and listener file
-antlr4 -Dlanguage=Python3 Expr.g4 -visitor;
+java -jar antlr-4.12.0-complete.jar -Dlanguage=Python3 Expr.g4 -visitor;
 #execute main
 python3 main.py Expr.g4
 #install bindings for AST
