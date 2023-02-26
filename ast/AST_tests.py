146f82b5adc21464dffa990f5ec0a52b9c627a0f
SaidDeWolf <saiddewolf@gmail.com>
2023-02-26 16:54:12 +0100

    DONE: - first setup of the AST-class for mathematical operations - generation of dot-file for the AST

---
+++ ast/AST_tests.py
@@ -0,0 +1,67 @@
+import unittest
+import AST
+import node
+
+
+class nodeTestCase(unittest.TestCase):
+    def test_getId(self):
+        testNode = node.AST_node()
+        testNode.setLevel(12)
+        testNode.setNumber(55)
+        self.assertEqual(testNode.getId(), "12.55", "should be 12.55")
+
+    def test_setNodeIds(self):
+        ast1 = AST.AST()
+        ast2 = AST.AST()
+
+        leaf1 = node.Value(5)
+        leaf2 = node.Value(22)
+        leaf3 = node.Value(-79)
+        mult = node.BinaryOperator("*", leaf1, leaf2)
+        neg = node.UnaryOperator("-", leaf3)
+        add = node.BinaryOperator("+", mult, neg)
+        ast1.setRoot(add)
+        ast1.setNodeIds(ast1.root)
+
+        leaf4 = node.Value(5)
+        leaf5 = node.Value(22)
+        leaf6 = node.Value(-79)
+        leaf4.setLevel(2)
+        leaf4.setNumber(2)
+        leaf5.setLevel(2)
+        leaf5.setNumber(3)
+        leaf6.setLevel(2)
+        leaf6.setNumber(5)
+        mult2 = node.BinaryOperator("*", leaf4, leaf5)
+        mult2.setLevel(1)
+        mult2.setNumber(1)
+        neg2 = node.UnaryOperator("-", leaf6)
+        neg2.setLevel(1)
+        neg2.setNumber(4)
+        add2 = node.BinaryOperator("+", mult2, neg2)
+        add2.setLevel(0)
+        add2.setNumber(0)
+        ast2.setRoot(add2)
+
+        self.assertEqual(ast1, ast2, "both ASTs should be the same")
+
+    def test_generateDot(self):
+        ast = AST.AST()
+
+        leaf1 = node.Value(5)
+        leaf2 = node.Value(22)
+        leaf3 = node.Value(-79)
+        mult = node.BinaryOperator("*", leaf1, leaf2)
+        neg = node.UnaryOperator("-", leaf3)
+        add = node.BinaryOperator("+", mult, neg)
+        ast.setRoot(add)
+        ast.setNodeIds(ast.root)
+
+        dot = ast.generateDot()
+        expected = "graph ast {\n0.0 [label=\"Binary operator: +\"]\n1.1 [label=\"Binary operator: *\"]\n2.2 " \
+                   "[label=\"Literal: 5\"]\n2.3 [label=\"Literal: 22\"]\n1.4 [label=\"Unary operator: -\"]\n2.5 " \
+                   "[label=\"Literal: -79\"]\n\n0.0--1.1\n0.0--1.4\n1.1--2.2\n1.1--2.3\n1.4--2.5\n}"
+        self.assertEqual(dot, expected)
+
+if __name__ == '__main__':
+    unittest.main()
--- ast/node.py
+++ ast/node.py
@@ -1,17 +1,84 @@
-class ast_node():
-    pass
+class AST_node():
+    number = None
+    level = None
 
-class value(ast_node):
-    def __init__(self, input):
-        self.value = input
+    def getId(self):
+        return str(self.level) + "." + str(self.number)
 
-class binaryOperator(ast_node):
-    def __int__(self, oper, value1, value2):
+    def setNumber(self, number):
+        self.number = number
+
+    def setLevel(self, level):
+        self.level = level
+
+
+class Value(AST_node):
+    def __init__(self, lit):
+        self.value = lit
+
+    def __eq__(self, other):
+        if not isinstance(other, Value):
+            return False
+        return self.value == other.value
+
+    def getValue(self):
+        return self.value
+
+    def getLabel(self):
+        if isinstance(self.value, int):
+            return "\"Literal: " + str(self.value) + "\""
+        elif isinstance(self.value, str):
+            return "\"Literal: " + self.value + "\""
+
+
+class BinaryOperator(AST_node):
+    def __init__(self, oper, value1, value2):
         self.operator = oper
         self.leftSide = value1
         self.rightSide = value2
 
-class unaryOperator(ast_node):
-    def __int__(self, oper, input):
+    def __eq__(self, other):
+        if not isinstance(other, BinaryOperator):
+            return False
+        return self.operator == other.operator and self.leftSide == other.leftSide and self.rightSide == other.rightSide
+
+    def getValue(self):
+        return self.operator
+
+    def getLabel(self):
+        return "\"Binary operator: " + self.operator + "\""
+
+
+class UnaryOperator(AST_node):
+    def __init__(self, oper, lit):
         self.operator = oper
-        self.value = input
+        self.value = lit
+
+    def __eq__(self, other):
+        if not isinstance(other, UnaryOperator):
+            return False
+        return self.operator == other.operator and self.value == other.value
+
+    def getValue(self):
+        return self.operator
+
+    def getLabel(self):
+        return "\"Unary operator: " + self.operator + "\""
+
+
+class LogicalOperator(AST_node):
+    def __init__(self, oper, value1, value2):
+        self.operator = oper
+        self.leftSide = value1
+        self.rightSide = value2
+
+    def __eq__(self, other):
+        if not isinstance(other, LogicalOperator):
+            return False
+        return self.operator == other.operator and self.leftSide == other.leftSide and self.rightSide == other.rightSide
+
+    def getValue(self):
+        return self.operator
+
+    def getLabel(self):
+        return "\"Logical operator: " + self.operator + "\""
