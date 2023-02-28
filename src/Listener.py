dba0e63c7be8916a72e33ffa15ecb19ccd729976
judithher <Judith.Hershko@student.uantwerpen.be>
2023-02-28 20:25:35 +0100

    tijdelijk2.0

--- src/Listener.py
+++ src/Listener.py
@@ -12,6 +12,7 @@ class Expression(ExpressionListener):
         self.asT=create_tree()
         self.count=0
         self.nr=0
+        self.node=None
     def enterExpr(self, ctx):
         self.count+=1
         self.nr+=1
@@ -19,12 +20,14 @@ class Expression(ExpressionListener):
         #print(ctx.getToken(int))
         if(ctx.binop()):
             print("val:"+ ctx.expr(0).getText()+ "op:" +ctx.binop().getText()+"val2:"+ctx.expr(1).getText() )
-            node=BinaryOperator(ctx.binop().getText(),ctx.expr(0).getText(),ctx.expr(1).getText())
-            node.setNumber(self.nr)
-            node.setLevel(self.count)
-            self.asT.setNodeIds(node)
+            self.node=BinaryOperator(ctx.binop().getText(),ctx.expr(0).getText(),ctx.expr(1).getText())
+            #node=BinaryOperator("+",3,23)
+            self.node.setNumber(0)
+            self.node.setLevel(1)
     def exitExpr(self, ctx):
         self.count = 1
+        self.asT.setRoot(self.node)
+
     def enterDec(self, ctx):
         print("enter declaration is: " +ctx.getText())
     def enterBinop(self, ctx):
