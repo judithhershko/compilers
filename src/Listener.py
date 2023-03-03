302aba6160c2f753952226063a33446be2c69597
judithher <Judith.Hershko@student.uantwerpen.be>
2023-03-03 12:37:00 +0100

    probleem geen output

--- src/Listener.py
+++ src/Listener.py
@@ -7,22 +7,27 @@ def create_tree():
     expression_tree.setRoot(None)
     return expression_tree
 
+
 class Expression(ExpressionListener):
     def __init__(self):
-        self.asT=create_tree()
-        self.current=None
-        self.parent=None
+        print("enter expression")
+        self.asT = create_tree()
+        self.current = None
+        self.parent = None
+
     def enterExpr(self, ctx):
         print("enter expression is:" + ctx.getText())
-        #print(ctx.getToken(int))
+        # print(ctx.getToken(int))
 
-        if(ctx.binop()):
+        if (ctx.binop()):
             pass
+
     def exitExpr(self, ctx):
         self.count = 1
         self.asT.setRoot(self.node)
 
     def enterDec(self, ctx):
-        print("enter declaration is: " +ctx.getText())
+        print("enter declaration is: " + ctx.getText())
+
     def enterBinop(self, ctx):
         print("enter declaration is: " + ctx.getText())
