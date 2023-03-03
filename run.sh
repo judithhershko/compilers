668e30275eb0f4fb9c0213b679265257958db76d
judithher <Judith.Hershko@student.uantwerpen.be>
2023-03-03 12:05:57 +0100

    wtf

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 # start folder=compilers
 export PYTHONPATH=$PYTHONPATH:src
 
-
+if [[ ! -f "antlr-4.12.0-complete.jar" ]]; then
+  wget https://www.antlr.org/download/antlr-4.12.0-complete.jar
+fi
 #generate visitor and listener file
 java -jar antlr-4.12.0-complete.jar -o generated -visitor -listener -Dlanguage=Python3 input/Expression.g4;
 #execute main
--- src/Listener.py
+++ src/Listener.py
@@ -45,6 +45,7 @@ class Expression(ExpressionListener):
         self.tijdL = None
         self.tijdR = None
 
+
     def search_left(self, left: BinaryOperator):
         if left.leftChild is None:
             return left
@@ -61,97 +62,8 @@ class Expression(ExpressionListener):
         self.trees.append(ast)
 
     def enterExpr(self, ctx):
-        self.count += 1
-        self.nr += 1
-        # print(ctx.getToken(int))
-        if ctx.binop():
-            self.count = 1
-            tnode = BinaryOperator(ctx.getText())
-            self.left = True
-            if self.root:
-                self.parent = tnode
-                self.root = False
-                self.left = True
-                self.asT.setRoot(self.parent)
-                self.asT.setNodeIds(self.asT.root)
-                print(self.asT.root.operator)
-
-            elif self.parent.leftChild and not self.parent.rightChild and self.lParent is None:
-                self.parent.setLeftChild(tnode)
-                self.lParent = self.parent
-                self.asT.setRoot(self.parent)
-                self.asT.setNodeIds(self.root)
-                print("l", self.c_level,"left")
-                if not has_children(ctx):
-                    self.left = False
-                    self.right = True
-            elif self.left and not self.right:
-                self.lParent.setLeftChild(tnode)
-                self.asT.setRoot(self.lParent)
-                """
-                probleem
-                """
-                self.lParent = self.lParent.leftChild
-                if not has_children(ctx):
-                    self.left = False
-                    self.right = True
-
-            elif self.right and self.lParent is None:
-                self.parent.setRightChild(tnode)
-                self.asT.setRoot(self.parent)
-                self.rParent = self.parent.rightChild
-                if not has_children(ctx):
-                    self.right = False
-                    self.left = True
-
-            self.asT.setRoot(tnode)
-        elif self.left:
-            print("left nr:", nr_children(ctx))
-            lnode = BinaryOperator(ctx.getText())
-            if self.parent.leftChild is None:
-                self.parent.setLeftChild(lnode)
-                self.asT.setRoot(self.parent)
-                if nr_children(ctx) == 1:
-                    self.left = False
-                    self.right = True
-            elif self.rParent is not None:
-                self.rParent.setLeftChild(lnode)
-                self.parent.rightChild=self.rParent
-                self.asT.setRoot(self.parent)
-                if has_children(ctx):
-                    print("hier geraakt")
-                    self.parent=self.rParent
-                    self.rParent=None
-                    ###########
-            elif self.parent.leftChild.leftChild is None:
-                self.parent.leftChild.setLeftChild(lnode)
-                self.asT.setRoot(self.parent)
-                self.left = False
-                self.right = True
-
-        elif self.right:
-            """
-            find current level
-            """
-            if self.parent.leftChild is not None and self.parent.leftChild.leftChild is not None:
-                print("tot hier")
-                rnode = BinaryOperator(ctx.getText())
-                self.parent.leftChild.setRightChild(rnode)
-                self.asT.setRoot(self.parent)
-                self.left = True
-                self.right = False
-            if self.parent.leftChild is not None and self.parent.leftChild.leftChild is None:
-                rnode = BinaryOperator(ctx.getText())
-                self.parent.setRightChild(rnode)
-                self.asT.setRoot(self.parent)
-                print(
-                    "right: root" + self.asT.root.operator + "left=" + self.asT.root.leftChild.operator + "right=" + self.asT.root.rightChild.operator)
-                if has_children(ctx):
-                    self.c_level+=1
-                    self.rParent = self.parent.rightChild
-                    self.lParent = None
-                self.left = True
-                self.right = False
+        #print("expression entered:"+ ctx.getText())
+        print("has children/"+ has_children(ctx))
 
     def exitExpr(self, ctx):
         # print("leaving: " + ctx.getText())
