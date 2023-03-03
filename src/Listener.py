f4f3227510e75c5a38ea10b74f29c12d2297df93
judithher <Judith.Hershko@student.uantwerpen.be>
2023-03-03 12:17:03 +0100

    change listener

---
+++ src/Listener.py
@@ -0,0 +1,28 @@
+from ast.AST import *
+from generated.input.ExpressionListener import *
+
+
+def create_tree():
+    expression_tree = AST()
+    expression_tree.setRoot(None)
+    return expression_tree
+
+class Expression(ExpressionListener):
+    def __init__(self):
+        self.asT=create_tree()
+        self.current=None
+        self.parent=None
+    def enterExpr(self, ctx):
+        print("enter expression is:" + ctx.getText())
+        #print(ctx.getToken(int))
+
+        if(ctx.binop()):
+            pass
+    def exitExpr(self, ctx):
+        self.count = 1
+        self.asT.setRoot(self.node)
+
+    def enterDec(self, ctx):
+        print("enter declaration is: " +ctx.getText())
+    def enterBinop(self, ctx):
+        print("enter declaration is: " + ctx.getText())
