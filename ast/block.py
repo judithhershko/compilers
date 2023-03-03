855818d57113638a454b94f30b713b572479cf2e
SaidDeWolf <saiddewolf@gmail.com>
2023-03-03 14:25:55 +0100

    DONE: - made the fold function to fold mathematical operations - created classes: program, block, literalType and symbolTable
    TODO:
    - discuss different operators and how to group them
    - fill in the variables of the ast

--- ast/block.py
+++ ast/block.py
@@ -1,5 +1,6 @@
 import symbolTable
 import AST
+import sys
 
 class block():
     def __init__(self, parent):
@@ -11,5 +12,32 @@ class block():
     def getSymbolTable(self):
         return self.symbols
 
+    def getParent(self):
+        return self.parent
+
     def fillLiterals(self):
-        pass
+        variables = self.ast.getVariables()
+        notFound = []
+        values = dict()
+        for elem in variables:
+            temp = self.symbols.findSymbol(elem)
+            if temp:
+                values[elem] = temp
+            else:
+                notFound.append(elem)
+
+        current = self
+        while current.getParent() and notFound:
+            current = self.getParent()
+            variables = notFound
+            notFound = []
+            for elem in variables:
+                temp = current.symbols.findSymbol(elem)
+                if temp:
+                    values[elem] = temp
+                else:
+                    notFound.append(elem)
+
+        if notFound:
+            print("This variable was not declared", file=sys.stderr)
+
