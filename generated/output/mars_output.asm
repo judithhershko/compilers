f91ff6b1b50bf26c86fed414af2c2904dd7eebdb
judithher <Judith.Hershko@student.uantwerpen.be>
2023-05-08 15:02:02 +0200

    statr functions mips

--- generated/output/mars_output.asm
+++ generated/output/mars_output.asm
@@ -7,3 +7,25 @@ xx: .word 90
 y: .word -67195
 z: .word 1
 .text
+.globl main
+ff: 
+addi $sp, $sp, -4        # allocate space for arguments on stack
+sw $ra, 0($sp)           # save return address on stack
+sw $fp, -5($sp)          # save frame pointer on stack
+addi $fp, $sp, 4         # set up new frame pointer
+#fucntion parameters
+s.s $1, 4($fp)
+f: 
+addi $sp, $sp, -8        # allocate space for arguments on stack
+sw $ra, 4($sp)           # save return address on stack
+sw $fp, -1($sp)          # save frame pointer on stack
+addi $fp, $sp, 8         # set up new frame pointer
+#fucntion parameters
+sw $1, 8($fp)
+s.s $1, 12($fp)
+main: 
+addi $sp, $sp, 0        # allocate space for arguments on stack
+sw $ra, -4($sp)           # save return address on stack
+sw $fp, -9($sp)          # save frame pointer on stack
+addi $fp, $sp, 0         # set up new frame pointer
+#fucntion parameters
--- src/MIPS/Mips.py
+++ src/MIPS/Mips.py
@@ -8,6 +8,9 @@ class Mips:
         self.output = ""
         self.data = ".data\n"
         self.text = ".text\n"
+        self.text += ".globl main\n"
+        # counter for temporary registers
+        self.temp_count = 0
 
     def write_to_file(self, filename: str):
         text_file = open(filename, "w")
@@ -30,6 +33,18 @@ class Mips:
             else:
                 content = "1"
         return content
+
+    def get_sw_type(self, value: Value):
+        if isinstance(value, Pointer):
+            return "sw"
+        if value.getType() == LiteralType.INT:
+            return "sw"
+        if value.getType() == LiteralType.FLOAT:
+            return "s.s"
+        if value.getType() == LiteralType.BOOL:
+            return "s.b"
+    def get_register(self,v:Value):
+        return 1
 
     def global_declaration(self, declaration: Declaration):
         content = self.get_value_content(declaration.rightChild)
@@ -58,10 +73,57 @@ class Mips:
             elif isinstance(tree.root, Array):
                 self.global_array(tree.root)
             elif isinstance(tree.root, Comment):
-                #self.global_comment(tree.root)
+                # self.global_comment(tree.root)
                 pass
         self.output += self.data
         self.output += self.text
 
     def transverse_function(self, scope: Scope):
-        pass
+        self.text += scope.f_name + ": \n"
+        self.function_parameters(scope.parameters)
+
+    def function_parameters(self, parameters):
+        # function start
+        p = -1 * len(parameters) * 4
+        self.text += "addi $sp, $sp, {}        # allocate space for arguments on stack\n".format(p)
+        p = -1 * p - 4
+        self.text += "sw $ra, {}($sp)           # save return address on stack\n".format(p)
+        p = p - 5
+        self.text += "sw $fp, {}($sp)          # save frame pointer on stack\n".format(p)
+        p = len(parameters) * 4
+        self.text += "addi $fp, $sp, {}         # set up new frame pointer\n".format(p)
+        self.text += "#fucntion parameters\n"
+        # save values
+        for par in parameters:
+            self.text += "{} ${}, {}($fp)\n".format(self.get_sw_type(parameters[par]), self.get_register(parameters[par]), p)
+            p += 4
+        # save addresses
+
+        # function body
+
+        # function end
+
+        """
+        
+f:
+    addi $sp, $sp, -16        # allocate space for 4 arguments on stack
+    sw $ra, 12($sp)           # save return address on stack
+    sw $fp, 8($sp)            # save frame pointer on stack
+    addi $fp, $sp, 16         # set up new frame pointer
+    sw $a0, 16($fp)           # save value of x on stack
+    s.s $f12, 20($fp)         # save value of y on stack
+    sb $a2, 24($fp)           # save value of z on stack
+    sw $a1, 28($fp)           # save address of xp on stack
+    sw $a3, 32($fp)           # save address of fp on stack
+    sw $a2, 36($fp)           # save address of zp on stack
+
+    # function body
+    li $v0, 1                 # return 1
+    
+    # function epilogue
+    lw $ra, 12($sp)           # restore return address from stack
+    lw $fp, 8($sp)            # restore frame pointer from stack
+    addi $sp, $sp, 16         # deallocate space for 4 arguments on stack
+    jr $ra                    # return to caller
+
+        """
