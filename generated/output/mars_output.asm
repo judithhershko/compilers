d83af589814135f5732854331b6f3c092240c002
SaidDeWolf <saiddewolf@gmail.com>
2023-05-18 11:47:48 +0200

    DONE: - fixed print and scan folding

--- generated/output/mars_output.asm
+++ generated/output/mars_output.asm
@@ -2,19 +2,48 @@
 .text
 .globl main
 j main
-#//intmain(){intz=2;{scanf(" val is: %d and %d",z*z,z);}return0;}
+#// Should print the numbers: 42 42 43 43 44 44 45 45
+#//intmain(){intx=0;int*xp=&x;*xp=42;int**p=&xp;inty=1;int*yp=&y;*yp=5;return1;}
 main: 
  sw	$fp, 0($sp)
 move	$fp, $sp
-subu	$sp, $sp,12
+subu	$sp, $sp,28
 sw	$ra, -4($fp)
 sw	$s0, -8($fp)
-#//intz=2
+sw	$s1, -12($fp)
+sw	$s2, -16($fp)
+sw	$s3, -20($fp)
+sw	$s4, -24($fp)
+#//intx=0
 lw  $s0, -8($fp)
-ori $s0,$0,2
+ori $s0,$0,0
 sw  $s0, -8($fp)
-#//scanf(" val is: %d and %d",z*z,z)
-li $v0, 0
+#//int*xp=&x
+lw  $s1, -12($fp)
+sw $s1, -8($fp)
+#//*xp=42
+lw  $s1, -12($fp)
+ori $s1,$0,42
+sw  $s1, -12($fp)
+#//int**p=&xp
+lw  $s2, -16($fp)
+sw $s2, -12($fp)
+#//inty=1
+lw  $s3, -20($fp)
+ori $s3,$0,1
+sw  $s3, -20($fp)
+#//int*yp=&y
+lw  $s4, -24($fp)
+sw $s4, -20($fp)
+#//*yp=5
+lw  $s4, -24($fp)
+ori $s4,$0,5
+sw  $s4, -24($fp)
+li $v0, 1
+lw $s4, -24($fp)
+lw $s3, -20($fp)
+lw $s2, -16($fp)
+lw $s1, -12($fp)
 lw $s0, -8($fp)
 lw	$ra, -4($fp)
 move	$sp, $fp
--- input/input.c
+++ input/input.c
@@ -1,16 +1,16 @@
-#include "stdio.h";
-int y = 1;
+#include <stdio.h>
+
+// Should print the numbers: 42 42 43 43 44 44 45 45
+
 int main(){
-int z = 2;
-{
-    int x = 3;
-    {
-        int u = 4;
-        {
-            int w = 5;
-        }
-    }
-	printf(" val is: %d and %d",z*z, z);
-}
-return 0;
+	int x = 0;
+	int* xp = &x;
+	*xp = 42;
+	int** p = &xp;
+
+	int y = 1;
+	int* yp = &y;
+	*yp = 5;
+
+	return 1;
 }
